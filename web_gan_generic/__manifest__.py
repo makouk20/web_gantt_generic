c2867e2240a3e5fa150afad19148f00a94a5550b
Turki <makouk22@gmail.com>
2020-12-20 22:05:40 +0100

    #

--- web_gan_generic/__manifest__.py
+++ web_gan_generic/__manifest__.py
@@ -1,33 +1,23 @@
 # -*- coding: utf-8 -*-
 {
     'name': "web_gan_generic",
-
     'summary': """
-        Short (1 phrase/line) summary of the module's purpose, used as
-        subtitle on modules listing or apps.openerp.com""",
-
+        This module allow you to display any field you want in the gantt view""",
     'description': """
-        Long description of module's purpose
+        This module allow you to display any field you want in the gantt view
     """,
-
     'author': "Maki Turki",
-    'website': "http://www.yourcompany.com",
-
     'category': 'sale',
     'version': '1.0',
     'application': True,
-
     # any module necessary for this one to work correctly
-    'depends': ['base', 'web_gantt','project_enterprise','web'],
-
+    'depends': ['base', 'web_gantt', 'project_enterprise', 'web'],
     # always loaded
     'data': [
         'views/project_gantt.xml',
         'views/assets.xml',
-
     ],
     'qweb': [
         'static/src/xml/web_gantt.xml',
     ]
-
 }
